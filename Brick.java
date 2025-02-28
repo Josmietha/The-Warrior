134d576f46e4e2d8c2f121dbe330617f9ededddc
Josmietha <147175674+Josmietha@users.noreply.github.com>
2025-02-28 22:17:32 +0530

    Add files via upload

---
+++ Brick.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Brick here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Brick extends Size
+{
+    /**
+     * Act - do whatever the Brick wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Brick()
+    {
+        super(3,3);
+        setRotation(40);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
