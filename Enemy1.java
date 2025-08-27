4f5922536289be8d21238f429148a18acac36441
Josmietha <147175674+Josmietha@users.noreply.github.com>
2025-08-27 07:38:47 +0530

    Update Enemy1.java

--- Enemy1.java
+++ Enemy1.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Enemy1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Enemy1 extends Size
 {
     /**
