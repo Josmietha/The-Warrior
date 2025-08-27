f8e466051ca7188f2780f5c2d30a8b0cbd3cb456
Josmietha <147175674+Josmietha@users.noreply.github.com>
2025-08-27 07:37:35 +0530

    Update Bullet1.java

--- Bullet1.java
+++ Bullet1.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Bullet1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Bullet1 extends Size
 {
     /**
